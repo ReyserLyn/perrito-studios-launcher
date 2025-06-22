cac3bf4c20a6d3d21a7129ef1bd0b9b119240674
Reyser Zapata <reyserlyn@gmail.com>
2025-06-22 05:04:38 -0500

    Eliminación de métodos obsoletos en la interfaz SystemAPI y actualización de la política de seguridad de contenido en index.html para incluir nuevas fuentes.

--- src/preload/system/types.ts
+++ src/preload/system/types.ts
@@ -1,7 +1,5 @@
 export default interface SystemAPI {
   trashItem: (filePath: string) => Promise<any>
-  showToast: (type: string, message: string) => void
-  ping: () => void
 
   // Distribution events
   onDistributionIndexDone: (callback: (result: boolean) => void) => void
