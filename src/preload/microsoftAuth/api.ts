d8413bdcb004816e8eac863f79eec0c9a2930c47
Reyser Zapata <reyserlyn@gmail.com>
2025-06-23 01:04:29 -0500

    Refactorización de la API de autenticación de Microsoft: se reemplazaron los strings de los opcodes por constantes definidas en el módulo correspondiente, mejorando la legibilidad y mantenibilidad del código.

--- src/preload/microsoftAuth/api.ts
+++ src/preload/microsoftAuth/api.ts
@@ -1,26 +1,27 @@
 import { ipcRenderer } from 'electron'
+import { MSFT_OPCODE } from '../../main/constants/ipc'
 
 export const microsoftAuthAPI = {
   openLogin: (successCallback?: boolean, onCloseCallback?: boolean) =>
-    ipcRenderer.send('MSFT_AUTH_OPEN_LOGIN', successCallback, onCloseCallback),
+    ipcRenderer.send(MSFT_OPCODE.OPEN_LOGIN, successCallback, onCloseCallback),
   openLogout: (uuid: string, isLastAccount: boolean) =>
-    ipcRenderer.send('MSFT_AUTH_OPEN_LOGOUT', uuid, isLastAccount),
+    ipcRenderer.send(MSFT_OPCODE.OPEN_LOGOUT, uuid, isLastAccount),
 
   // Event listeners
   onLoginReply: (callback: (type: string, data: any, closeCallback?: boolean) => void) => {
-    ipcRenderer.on('MSFT_AUTH_REPLY_LOGIN', (_, type, data, closeCallback) =>
+    ipcRenderer.on(MSFT_OPCODE.REPLY_LOGIN, (_, type, data, closeCallback) =>
       callback(type, data, closeCallback)
     )
   },
   onLogoutReply: (callback: (type: string, uuid?: string, isLastAccount?: boolean) => void) => {
-    ipcRenderer.on('MSFT_AUTH_REPLY_LOGOUT', (_, type, uuid, isLastAccount) =>
+    ipcRenderer.on(MSFT_OPCODE.REPLY_LOGOUT, (_, type, uuid, isLastAccount) =>
       callback(type, uuid, isLastAccount)
     )
   },
   removeLoginListener: () => {
-    ipcRenderer.removeAllListeners('MSFT_AUTH_REPLY_LOGIN')
+    ipcRenderer.removeAllListeners(MSFT_OPCODE.REPLY_LOGIN)
   },
   removeLogoutListener: () => {
-    ipcRenderer.removeAllListeners('MSFT_AUTH_REPLY_LOGOUT')
+    ipcRenderer.removeAllListeners(MSFT_OPCODE.REPLY_LOGOUT)
   }
 }
