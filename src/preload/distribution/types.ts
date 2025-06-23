f5f70d505f5d2716edd1ecbc006aca3f9245e2a9
Reyser Zapata <reyserlyn@gmail.com>
2025-06-22 21:58:45 -0500

    Refactorización de la interfaz DistributionAPI: se introdujo un nuevo tipo GetDistributionResponse para mejorar la claridad en la respuesta de la función getDistribution, simplificando la gestión de errores y el manejo de datos.

--- src/preload/distribution/types.ts
+++ src/preload/distribution/types.ts
@@ -1,9 +1,9 @@
 import { HeliosDistribution } from 'perrito-core/common'
 
+type GetDistributionResponse =
+  | { success: true; distribution: HeliosDistribution }
+  | { success: false; error: string }
+
 export default interface DistributionAPI {
-  getDistribution: () => Promise<{
-    success: boolean
-    distribution: HeliosDistribution
-    error?: string
-  }>
+  getDistribution: () => Promise<GetDistributionResponse>
 }
