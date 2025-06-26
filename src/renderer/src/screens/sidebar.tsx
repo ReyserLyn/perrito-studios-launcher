b032580181d73078e255ed31b401ac2709b8853b
Reyser Zapata <reyserlyn@gmail.com>
2025-06-26 03:31:39 -0500

    Se añadió la dependencia "react-icons" en package.json y se eliminó "simple-icons". Se implementaron nuevos iconos sociales en el componente Sidebar.

--- src/renderer/src/screens/sidebar.tsx
+++ src/renderer/src/screens/sidebar.tsx
@@ -1,10 +1,16 @@
-import { Download, Home, Settings } from 'lucide-react'
+import { Settings } from 'lucide-react'
+
+import { FaDiscord, FaFacebook, FaTiktok, FaXTwitter } from 'react-icons/fa6'
 import logo from '../assets/images/logos/Cua_Color_LBlanco.webp'
 import { Button } from '../components/ui/button'
 import { useNavigationStore } from '../stores/use-navigation-store'
 
 export function Sidebar() {
   const { goToConfig } = useNavigationStore()
+
+  const openSocialLink = (url: string) => {
+    window.api.system.openExternal?.(url)
+  }
 
   return (
     <div className="h-screen w-20 bg-[#151126] border-r border-[#2c1e4d] flex flex-col">
@@ -14,26 +20,46 @@ export function Sidebar() {
       </div>
 
       {/* Main Navigation */}
-      <nav className="flex-1 px-2 flex flex-col ">
+      <nav className="flex-1 px-2 flex flex-col">
         <div className="gap-2 flex flex-col items-center">
           <Button
             variant="ghost"
             size="icon"
-            title="Inicio"
-            asChild
+            title="TikTok"
+            onClick={() => openSocialLink('https://www.tiktok.com/@perritostudios')}
             className="bg-[#1d1332] border-[#2c1e4d] border-1 p-3 h-11 w-11"
           >
-            <Home size={5} />
+            <FaTiktok size={5} />
           </Button>
 
           <Button
             variant="ghost"
             size="icon"
-            title="Descargas"
-            asChild
+            title="Twitter/X"
+            onClick={() => openSocialLink('https://x.com/PerritoStudios')}
             className="bg-[#1d1332] border-[#2c1e4d] border-1 p-3 h-11 w-11"
           >
-            <Download size={5} />
+            <FaXTwitter size={5} />
+          </Button>
+
+          <Button
+            variant="ghost"
+            size="icon"
+            title="Facebook"
+            onClick={() => openSocialLink('https://x.com/PerritoStudios')}
+            className="bg-[#1d1332] border-[#2c1e4d] border-1 p-3 h-11 w-11"
+          >
+            <FaFacebook size={5} />
+          </Button>
+
+          <Button
+            variant="ghost"
+            size="icon"
+            title="Discord"
+            onClick={() => openSocialLink('https://discord.gg/BGyGxeaq27')}
+            className="bg-[#1d1332] border-[#2c1e4d] border-1 p-3 h-11 w-11"
+          >
+            <FaDiscord size={5} />
           </Button>
         </div>
       </nav>
